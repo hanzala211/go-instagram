dceff79550940702ca6dcefaad23755704f45b6d
Hanzala Javed <hanzalaoc211@gmail.com>
2025-08-27 00:36:07 +0500

    fixes

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 
 require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
 	github.com/go-pg/zerochecker v0.2.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
--- router/router.go
+++ router/router.go
@@ -2,12 +2,18 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/hanzala211/instagram/internal/api/handler"
 	"github.com/hanzala211/instagram/middlewares"
 )
 
 func SetupRouter(userHandler *handler.UserHandler) *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+	}))
 	r.Route("/api/v1", func(r chi.Router) {
 		r.Route("/auth", func(u chi.Router) {
 			u.Post("/signup", userHandler.Signup)
